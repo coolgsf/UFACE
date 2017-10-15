9dbb688b22af6c4a80b65c85795d677948982d12
coolgsf <coolgsf@hotmail.com>
2017-10-15 23:31:00 +0800

    add new case

--- Superhero_Nuisance.py
+++ Superhero_Nuisance.py
@@ -41,7 +41,6 @@ def fix_machine(debris, product):
     else:
         return "Give me something that's not useless next time."
 
-
 ### TEST CASES ###
 print "Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') #== "Give me something that's not useless next time."
 print "Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') #== 'Udacity'
---
+++ test.py
@@ -0,0 +1,18 @@
+#def fix_machine(debris, product):
+    ### WRITE YOUR CODE HERE ###
+
+    #p = debris.find(product[n])
+n = -1
+l = 'abc'
+print len(l)
+
+
+   # while p == -1:
+    #    if product.find[n - 1] == -1:
+    #        print product
+    #    else:
+     #       print "Give me something that's not useless next time.
+
+
+### TEST CASES ###
+#print "Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity')
